f0adb6d561b80f49317dccf89917da93c9f69c72
Biniam Hagos <b.hagos17@gmail.com>
2018-03-30 17:21:21 -0700

    worked out game logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 console.log("up and running....")
-var cardOne = "Queen";
-var cardTwo = "King";
-var cardThree = "Jack";
-var cardFour = "Ace";
-console.log("card fliped " + cardFour);
+var cards = ["Queen", "Queen", "King", "King"]
+var cardsInPlay = [];
+
+var checkForMatch = function(){
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match!");
+        } else {
+        alert("Sorry, try again.");
+        }
+}
+var flipCard = function(cardId){
+    console.log("user fliped " + cards[cardId])
+    cardsInPlay.push(cards[cardId]);
+    if (cardsInPlay.length >= 2){
+        checkForMatch();
+        cardsInPlay = [];
+    }
+    
+    
+}
