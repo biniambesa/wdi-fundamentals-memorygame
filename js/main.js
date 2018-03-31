6163153312ace2e1e6618937bb5f2d1491d392db
Biniam Hagos <b.hagos17@gmail.com>
2018-03-30 17:57:55 -0700

    added more  game logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 console.log("up and running....")
-var cards = ["Queen", "Queen", "King", "King"]
+var cards = [
+    
+        { rank: 'Queen',
+          suit: 'Hearts',
+          image: "images/queen-of-hearts.png"
+        },  
+        { rank: 'Queen',
+          suit: 'Diamonds',
+          image: "images/queen-of-diamonds.png"
+        },
+        { rank: 'King',
+          suit: 'Hearts',
+          image: "images/king-of-hearts.png"
+        }, 
+        { rank: 'King',
+          suit: 'Diamonds',
+          image: "images/king-of-diamonds.png"
+      },
+]
 var cardsInPlay = [];
 
 var checkForMatch = function(){
@@ -10,7 +28,7 @@ var checkForMatch = function(){
         }
 }
 var flipCard = function(cardId){
-    console.log("user fliped " + cards[cardId])
+    console.log("user fliped " + cards[cardId].rank + " of " + cards[cardId].suit)
     cardsInPlay.push(cards[cardId]);
     if (cardsInPlay.length >= 2){
         checkForMatch();
